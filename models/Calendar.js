2786a8995469f42d648c2ff1813b376b3d98aff0
Ulises Ros <ulisesros70@gmail.com>
2024-10-22 18:54:21 -0300

    incorporacion de fuerza base integral

--- models/Calendar.js
+++ models/Calendar.js
@@ -3,72 +3,92 @@ const mongoose = require('mongoose');
 const calendarSchema = new mongoose.Schema({
     lunes: {
         mañana: {
+            7: [String],
             8: [String],
             9: [String],
             10: [String],
-            11: [String]
+            11: [String],
+            12: [String],
         },
         tarde: {
+            15: [String],
             16: [String],
             17: [String],
             18: [String],
-            19: [String]
+            19: [String],
+            20: [String],
         }
     },
     martes: {
         mañana: {
+            7: [String],
             8: [String],
             9: [String],
             10: [String],
-            11: [String]
+            11: [String],
+            12: [String],
         },
         tarde: {
+            15: [String],
             16: [String],
             17: [String],
             18: [String],
-            19: [String]
+            19: [String],
+            20: [String],
         }
     },
     miércoles: {
         mañana: {
+            7: [String],
             8: [String],
             9: [String],
             10: [String],
-            11: [String]
+            11: [String],
+            12: [String],
         },
         tarde: {
+            15: [String],
             16: [String],
             17: [String],
             18: [String],
-            19: [String]
+            19: [String],
+            20: [String],
         }
     },
     jueves: {
         mañana: {
+            7: [String],
             8: [String],
             9: [String],
             10: [String],
-            11: [String]
+            11: [String],
+            12: [String],
         },
         tarde: {
+            15: [String],
             16: [String],
             17: [String],
             18: [String],
-            19: [String]
+            19: [String],
+            20: [String],
         }
     },
     viernes: {
         mañana: {
+            7: [String],
             8: [String],
             9: [String],
             10: [String],
-            11: [String]
+            11: [String],
+            12: [String],
         },
         tarde: {
+            15: [String],
             16: [String],
             17: [String],
             18: [String],
-            19: [String]
+            19: [String],
+            20: [String],
         }
     },
     sábado: {
